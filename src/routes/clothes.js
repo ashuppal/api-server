97ed7ce5dc44297edc3c14716ee1203efa3d2098
Ash Uppal <ashuppal@gmail.com>
2023-02-16 22:59:12 -0800

    render implementation working

--- src/routes/clothes.js
+++ src/routes/clothes.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const express = require ('express');
-const {foodModel} = require ('../models');
+const {clothesModel} = require ('../models');
 const router = express.Router ();
 
 router.get ('/clothes', async (req, res, next) => {
-  const food = await foodModel.findAll ();
+  const food = await clothesModel.findAll ();
   res.status (200).send (food);
 });
 
@@ -13,25 +13,25 @@ router.get ('/clothes/:id', async (req, res, next) => {
   const {id} = req.params;
   console.log ('cloth id is: ', id);
 
-  const singleFoodItem = await foodModel.findByPk (id);
+  const singleFoodItem = await clothesModel.findByPk (id);
   res.status (200).send (singleFoodItem);
 });
 
 router.post ('/clothes', async (req, res, next) => {
-  let newFoodItem = await foodModel.create (req.body);
+  let newFoodItem = await clothesModel.create (req.body);
   res.status (200).send (newFoodItem);
 });
 
 router.put ('/clothes/:id', async (req, res, next) => {
   const {id} = req.params;
-  const itemsModified = await foodModel.update (req.body, {where: {id}});
+  const itemsModified = await clothesModel.update (req.body, {where: {id}});
 
   res.status (200).send (itemsModified);
 });
 
 router.delete ('/clothes/:id', async (req, res, next) => {
   const {id} = req.params;
-  await foodModel.destroy ({where: {id}});
+  await clothesModel.destroy ({where: {id}});
   res.status (200).send ('deleted');
 });
 
